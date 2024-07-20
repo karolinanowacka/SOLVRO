03fc8f7929fde9df33b259de54e87f5d4b25c3bd
Karolina Nowacka <278427@student.pwr.edu.pl>
2024-07-20 17:05:01 +0200

    Loaded the provided data.

---
+++ SOLVRO_KN.py
@@ -0,0 +1,13 @@
+#SOLVRO KN
+import numpy as np
+
+#loading training data
+X_train=np.load('/Users/karolinanowacka/Downloads/solvro-rekrutacja-zimowa-ml-2022/X_train.npy')
+y_train=np.load('/Users/karolinanowacka/Downloads/solvro-rekrutacja-zimowa-ml-2022/y_train.npy')
+
+#loading validation data
+X_val=np.load('/Users/karolinanowacka/Downloads/solvro-rekrutacja-zimowa-ml-2022/X_val.npy')
+y_val=np.load('/Users/karolinanowacka/Downloads/solvro-rekrutacja-zimowa-ml-2022/y_val.npy')
+
+#loading test data
+X_test=np.load('/Users/karolinanowacka/Downloads/solvro-rekrutacja-zimowa-ml-2022/X_test.npy')
